17031f474d1f83e25e329033abb373211ad2cd51
Махетов Сергей <Sergey.Makhetov@centre-it.com>
2016-06-08 20:37:38 +0500

    Add maven routes

--- src/main/scala/FromFileToFileApp.scala
+++ src/main/scala/FromFileToFileApp.scala
@@ -7,17 +7,37 @@ import org.apache.camel.scala.dsl.builder.ScalaRouteBuilder
 
 
 
-object FromFileToFileApp{
-
+object FromFileToFileApp {//extends App {
   val context = new DefaultCamelContext
   context.addRoutes(FromFileToFileRoute)
+ // FromFileToFileRoute
+
+
+    object FromFileToFileRoute extends ScalaRouteBuilder(context) {
+      """file:inbox?charset=utf-8""" ==> {
+        to ("file:outbox?charset=Windows-1251")
+      }
+    }
   context.start
   Thread.currentThread.join
 
-  object FromFileToFileRoute extends ScalaRouteBuilder(context){
-    """file:inbox?charset=utf-8""" ==> {
-      to ("file:outbox?charset=Windows-1251")
-    }
-  }
+
+  //addRoutesToCamelContext(context)
 }
 
+//object FromFileToFileApp extends App {
+//  val context = new DefaultCamelContext
+//  // FromFileToFileRoute
+//  context.start
+//  Thread.currentThread.join
+//}
+//
+//
+//object FromFileToFileRoute extends ScalaRouteBuilder(FromFileToFileApp.context) {
+//  """file:inbox?charset=utf-8""" ==> {
+//    to ("file:outbox?charset=Windows-1251")
+//  }
+//  addRoutesToCamelContext(context)
+//}
+
+// context.addRoutes(new FromFileToFileRoute)
--- src/main/scala/FromHTTPToRedis.scala
+++ src/main/scala/FromHTTPToRedis.scala
@@ -7,7 +7,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 /**
   * Created by smakhetov on 07.06.2016.
   */
-object FromHTTPToRedis extends App{
+object FromHTTPToRedis{// extends App{
 
 
   val registry = new SimpleRegistry
@@ -22,20 +22,23 @@ object FromHTTPToRedis extends App{
 
   object FromHTTPToRedisRoute extends ScalaRouteBuilder(context) with Languages{
 
+    """quartz://groupName/timerName?cron=0+0/1+*+*+*+?""" ==> {
 
-
-    """quartz://groupName/timerName?cron=0/5+*+*+*+*+?""" ==> {
-
-//      val myProcessor = (exchange: Exchange) => {
-//        exchange.getOut.setHeader("qwer","retr")
-//      }
-      //to("""https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=""")
-      to("http:camel.apache.org/http4.html")
-      process((exchange: Exchange) => {exchange.getOut.setHeader("CamelRedis.Key",System.currentTimeMillis())
-      exchange.getOut.setHeader("CamelRedis.Value",exchange.getIn.getBody(classOf[String]))})
-      //setHeader("CamelRedis.Value","Rghty")
-      //setHeader("CamelRedis.Key",System.currentTimeMillis())
+      //val addr = "http:www.google.com/finance/info?q=NASDAQ%3aGOOG"
+      to("http://www.google.com/finance/info?q=CURRENCY%3aUSDRUB")
+      process((exchange: Exchange) => {
+        exchange.getOut.setHeader("CamelRedis.Key",System.currentTimeMillis())
+        exchange.getOut.setHeader("CamelRedis.Value",exchange.getIn.getBody(classOf[String]))
+      })
       to ("""spring-redis://172.16.7.58:6379?serializer=#stringSerializer""")
     }
   }
 }
+
+//      val myProcessor = (exchange: Exchange) => {
+//        exchange.getOut.setHeader("qwer","retr")
+//      }
+//to("""https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=""")
+//to("http:camel.apache.org/http4.html")
+//setHeader("CamelRedis.Value","Rghty")
+//setHeader("CamelRedis.Key",System.currentTimeMillis())
